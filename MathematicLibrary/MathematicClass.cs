c902a09ad7d61250b9429ccc4cdabee4c4fee5f8
Сын <iamvladgubarevofficial@gmail.com>
2022-11-24 22:35:00 +0500

    Сделан метод для суммы чисел.

---
+++ MathematicLibrary/MathematicClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicLibrary
+{
+    
+    public class MathematicClass
+    {
+        /// <summary>
+        /// Нахождение суммы двух чисел
+        /// </summary>
+        /// <param name="x">
+        /// Первое число
+        /// </param>
+        /// <param name="y">
+        /// Второе число
+        /// </param>
+        /// <returns>
+        /// Сумма чисел
+        /// </returns>
+        public static int  Sum(int x, int y)
+        {
+        return x + y;
+        }
+    }
+}
